2b23098279ef584b8dd1b42f25ac79b97bafe810
jake <jrstruck@ncsu.edu>
2019-01-07 18:02:56 -0500

    renamed project

--- portfoliowebsite/wsgi.py
+++ portfoliowebsite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for resumewebserver project.
+WSGI config for portfoliowebsite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'resumewebserver.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfoliowebsite.settings')
 
 application = get_wsgi_application()
