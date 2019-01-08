b911c2f0372f81510300d09b9ba294a984f45712
Jacob Struckmeyer <jake@localhost.localdomain>
2019-01-08 00:17:42 -0500

    added projects app to the list of installed apps, created initial models

--- portfoliowebsite/settings.py
+++ portfoliowebsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'projectsapp.apps.ProjectsappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- projectsapp/models.py
+++ projectsapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)    
+    biography = models.CharField(max_length=255)
+
+class Project(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    authors = models.ManyToManyField(Author)
+    github_link = models.CharField(max_length=255)
