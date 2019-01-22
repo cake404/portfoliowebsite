b418d1ff865db32bfde9a2ad2629fd958d663cdf
Jacob Struckmeyer <jake@localhost.localdomain>
2019-01-22 18:00:35 -0500

    Added a home page, and modified the index page to display all project info

--- projectsapp/urls.py
+++ projectsapp/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'projectsapp'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.ProjectDetailView.as_view(), name='project_detail'),
+    # path('<int:pk>/', views.ProjectDetailView.as_view(), name='project_detail'),
+    path('projectdetails', views.get_project_details, name="project_details"), 
     path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='author_detail')
 ]
--- projectsapp/views.py
+++ projectsapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Project, Author
 from django.views import generic
+from django.http import JsonResponse
+
+import json
 
 
 class IndexView(generic.ListView):
@@ -17,3 +20,18 @@ class ProjectDetailView(generic.DetailView):
 class AuthorDetailView(generic.DetailView):
     model = Author
     template_name = 'projectsapp/author_detail.html'
+
+def get_project_details(request):
+    id = request.GET.get('id', None)
+    project = Project.objects.get(pk=id)
+    project_json = {
+        'id' : project.id,
+        'title' : project.title,
+        'Authors' : project.authors_string(),
+        'description' : project.description, 
+        'is_school_project' : project.is_school_project,
+        'image_url' : project.picture_url
+    }
+    return JsonResponse(project_json)
+
+    
